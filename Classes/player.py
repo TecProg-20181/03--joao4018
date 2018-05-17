a5062424e218a0712e951d03a062a708dbbd5b7c
joao4018 <joao4018@gmail.com>
2018-05-17 16:14:39 -0300

    Add sistema de log

--- Classes/player.py
+++ Classes/player.py
@@ -1,8 +1,11 @@
+import logging
 import warnings
 class Player():
 
     def __init__(self):
-      self.__guesses = 8
+        self.__guesses = 8
+        logging.basicConfig(filename='logging.log',level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
     def __numberOfGuesses(self):
         print 'You can only play with words that have'
@@ -11,12 +14,16 @@ class Player():
         while True:
             try:
                 self.__guesses = int(raw_input('Choose the number of guesses to play: '))
+                self.logger.info('quantify guesses: %d', self.__guesses)
                 break
             except ValueError:
+                self.logger.error('Value entered is not number')
                 print "Oops!  That was no valid number.  Try again..."
                 print '----------------------------------------------'
         if(self.__guesses < 0):
-            warnings.warn("number")
+            self.logger.warn('Value entered is number less than or equal to 0')
+            print 'You entered that number ' + str(self.__guesses) + ' that is less than or equal to 0'
+            print '----------------------------------------------'
             return self.__numberOfGuesses()
         print 'Cool! You have', self.__guesses, 'guesses'
 
